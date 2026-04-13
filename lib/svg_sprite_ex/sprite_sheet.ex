5e8ef9c1caa5a7fa7efe8bddc34c6b53820b1dd1
Håvard Lindset <havard@givn.no>
2026-04-13 08:40:34 +0200

    Namespace internal SVG ids in sprite sheets

--- lib/svg_sprite_ex/sprite_sheet.ex
+++ lib/svg_sprite_ex/sprite_sheet.ex
@@ -1,9 +1,23 @@
 defmodule SvgSpriteEx.SpriteSheet do
   @moduledoc false
 
+  require Record
+
   alias SvgSpriteEx.Source
 
+  Record.defrecordp(
+    :xml_attribute,
+    Record.extract(:xmlAttribute, from_lib: "xmerl/include/xmerl.hrl")
+  )
+
+  Record.defrecordp(
+    :xml_element,
+    Record.extract(:xmlElement, from_lib: "xmerl/include/xmerl.hrl")
+  )
+
   @passthrough_attribute_exclusions MapSet.new(["height", "viewBox", "width", "xmlns"])
+  @local_fragment_href_attrs MapSet.new(["href", "xlink:href"])
+  @local_url_reference_pattern ~r/url\(\s*(['"]?)#([^)'" ]+)\1\s*\)/
 
   @doc """
   Builds a deterministic `<svg>` sprite sheet from logical SVG asset paths.
@@ -37,10 +51,8 @@ defmodule SvgSpriteEx.SpriteSheet do
   defp build_symbol!(%Source{
          name: normalized_name,
          attributes: attributes,
-         inner_content: inner_content
+         content_nodes: content_nodes
        }) do
-    # `inner_content` comes from `Source.read!/2`, which already parsed the SVG subtree.
-
     view_box = Map.get(attributes, "viewBox")
     sprite_id = Source.sprite_id_from_normalized(normalized_name)
     rendered_symbol_attrs = render_symbol_attrs(attributes)
@@ -50,10 +62,11 @@ defmodule SvgSpriteEx.SpriteSheet do
     end
 
     escaped_view_box = escape_xml_attr(view_box)
+    rewritten_content = rewrite_content_nodes!(normalized_name, content_nodes, sprite_id)
 
     """
     <symbol id="#{sprite_id}" viewBox="#{escaped_view_box}"#{rendered_symbol_attrs}>
-    #{inner_content}
+    #{rewritten_content}
     </symbol>
     """
   end
@@ -88,11 +101,195 @@ defmodule SvgSpriteEx.SpriteSheet do
     end
   end
 
+  defp rewrite_content_nodes!(normalized_name, content_nodes, sprite_id) do
+    id_map =
+      content_nodes
+      |> collect_local_ids()
+      |> Map.new(fn id -> {id, "#{sprite_id}-#{id}"} end)
+
+    content_nodes
+    |> rewrite_nodes!(normalized_name, id_map)
+    |> render_content_nodes()
+  end
+
+  defp collect_local_ids(nodes, acc \\ MapSet.new())
+
+  defp collect_local_ids([], acc), do: acc
+
+  defp collect_local_ids([node | rest], acc) do
+    acc =
+      if is_tuple(node) and tuple_size(node) > 0 and elem(node, 0) == :xmlElement do
+        node
+        |> xml_element(:attributes)
+        |> collect_ids_from_attributes(acc)
+        |> then(&collect_local_ids(xml_element(node, :content), &1))
+      else
+        acc
+      end
+
+    collect_local_ids(rest, acc)
+  end
+
+  defp collect_ids_from_attributes(attributes, acc) do
+    Enum.reduce(attributes, acc, fn attribute, collected_ids ->
+      if attribute_name(attribute) == "id" do
+        case attribute_value(attribute) do
+          "" -> collected_ids
+          value -> MapSet.put(collected_ids, value)
+        end
+      else
+        collected_ids
+      end
+    end)
+  end
+
+  defp rewrite_nodes!(nodes, normalized_name, id_map) do
+    Enum.map(nodes, &rewrite_node!(&1, normalized_name, id_map))
+  end
+
+  defp rewrite_node!(node, normalized_name, id_map) do
+    if is_tuple(node) and tuple_size(node) > 0 and elem(node, 0) == :xmlElement do
+      updated_attributes =
+        node
+        |> xml_element(:attributes)
+        |> Enum.map(&rewrite_attribute!(&1, normalized_name, id_map))
+
+      updated_content =
+        node
+        |> xml_element(:content)
+        |> rewrite_nodes!(normalized_name, id_map)
+
+      xml_element(node, attributes: updated_attributes, content: updated_content)
+    else
+      node
+    end
+  end
+
+  defp rewrite_attribute!(attribute, normalized_name, id_map) do
+    name = attribute_name(attribute)
+    value = attribute_value(attribute)
+
+    rewritten_value =
+      cond do
+        name == "id" ->
+          rewrite_local_id!(value, normalized_name, id_map)
+
+        MapSet.member?(@local_fragment_href_attrs, name) ->
+          rewrite_fragment_href!(value, name, normalized_name, id_map)
+
+        true ->
+          rewrite_url_references!(value, name, normalized_name, id_map)
+      end
+
+    if rewritten_value == value do
+      attribute
+    else
+      xml_attribute(attribute, value: String.to_charlist(rewritten_value))
+    end
+  end
+
+  defp rewrite_local_id!("", _normalized_name, _id_map), do: ""
+
+  defp rewrite_local_id!(value, normalized_name, id_map) do
+    Map.fetch!(id_map, value)
+  rescue
+    KeyError ->
+      raise ArgumentError,
+            "svg asset #{inspect(normalized_name)} references unknown local id #{inspect(value)}"
+  end
+
+  defp rewrite_fragment_href!(value, attr_name, normalized_name, id_map) do
+    trimmed_value = String.trim(value)
+
+    cond do
+      trimmed_value == "" ->
+        value
+
+      String.starts_with?(trimmed_value, "#") ->
+        "#" <>
+          rewrite_reference_target!(
+            String.trim_leading(trimmed_value, "#"),
+            attr_name,
+            normalized_name,
+            id_map
+          )
+
+      true ->
+        raise_unsupported_reference!(normalized_name, attr_name, value)
+    end
+  end
+
+  defp rewrite_url_references!(value, _attr_name, _normalized_name, _id_map)
+       when not is_binary(value),
+       do: value
+
+  defp rewrite_url_references!(value, attr_name, normalized_name, id_map) do
+    if String.contains?(value, "url(") do
+      unsupported_url_references =
+        Regex.replace(@local_url_reference_pattern, value, "")
+
+      if String.contains?(unsupported_url_references, "url(") do
+        raise_unsupported_reference!(normalized_name, attr_name, value)
+      end
+
+      rewritten_value =
+        Regex.replace(@local_url_reference_pattern, value, fn _, quote, target ->
+          escaped_quote = if quote == "", do: "", else: quote
+
+          "url(" <>
+            escaped_quote <>
+            "#" <>
+            rewrite_reference_target!(target, attr_name, normalized_name, id_map) <>
+            escaped_quote <> ")"
+        end)
+
+      rewritten_value
+    else
+      value
+    end
+  end
+
+  defp rewrite_reference_target!(target, attr_name, normalized_name, id_map) do
+    case Map.fetch(id_map, target) do
+      {:ok, rewritten_target} ->
+        rewritten_target
+
+      :error ->
+        raise ArgumentError,
+              "svg asset #{inspect(normalized_name)} references unknown local id #{inspect(target)} " <>
+                "from #{attr_name}"
+    end
+  end
+
+  defp raise_unsupported_reference!(normalized_name, attr_name, value) do
+    raise ArgumentError,
+          "svg asset #{inspect(normalized_name)} contains an unsupported reference in #{attr_name}: #{inspect(value)}"
+  end
+
+  defp render_content_nodes(nodes) do
+    nodes
+    |> :xmerl.export_simple_content(:xmerl_xml)
+    |> IO.iodata_to_binary()
+    |> String.trim()
+  end
+
   defp render_symbol_attrs(attributes) do
     attributes
     |> symbol_attributes()
     |> Enum.sort_by(fn {name, _value} -> name end)
     |> Enum.map_join("", fn {name, value} -> ~s( #{name}="#{escape_xml_attr(value)}") end)
+  end
+
+  defp attribute_name(attribute) do
+    attribute
+    |> xml_attribute(:name)
+    |> Atom.to_string()
+  end
+
+  defp attribute_value(attribute) do
+    attribute
+    |> xml_attribute(:value)
+    |> IO.iodata_to_binary()
   end
 
   defp escape_xml_attr(value) do
--- test/sprite_sheet_test.exs
+++ test/sprite_sheet_test.exs
@@ -106,6 +106,103 @@ defmodule SvgSpriteEx.SpriteSheetTest do
     end
   end
 
+  test "build namespaces local ids and rewrites url-based references inside each symbol" do
+    svg_source_root = unique_tmp_dir!("rewritten-refs")
+    File.mkdir_p!(Path.join(svg_source_root, "icons"))
+
+    File.write!(
+      Path.join(svg_source_root, "icons/complex.svg"),
+      """
+      <svg viewBox="0 0 24 24">
+        <defs>
+          <linearGradient id="paint">
+            <stop offset="0%" />
+          </linearGradient>
+          <clipPath id="clipper">
+            <rect x="0" y="0" width="24" height="24" />
+          </clipPath>
+          <mask id="masker">
+            <rect x="0" y="0" width="24" height="24" fill="white" />
+          </mask>
+          <filter id="blur">
+            <feGaussianBlur stdDeviation="1" />
+          </filter>
+          <marker id="arrow" viewBox="0 0 10 10" refX="5" refY="5" markerWidth="6" markerHeight="6">
+            <path d="M0 0 L10 5 L0 10z" />
+          </marker>
+        </defs>
+        <path
+          fill="url(#paint)"
+          clip-path="url(#clipper)"
+          mask="url(#masker)"
+          filter="url(#blur)"
+          marker-end="url(#arrow)"
+          d="M2 2h20v20H2z"
+        />
+      </svg>
+      """
+    )
+
+    sprite_sheet = SpriteSheet.build(["icons/complex"], source_root: svg_source_root)
+    sprite_id = Source.sprite_id("icons/complex", svg_source_root)
+
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-paint")
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-clipper")
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-masker")
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-blur")
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-arrow")
+    assert sprite_sheet =~ ~s|fill="url(##{sprite_id}-paint)"|
+    assert sprite_sheet =~ ~s|clip-path="url(##{sprite_id}-clipper)"|
+    assert sprite_sheet =~ ~s|mask="url(##{sprite_id}-masker)"|
+    assert sprite_sheet =~ ~s|filter="url(##{sprite_id}-blur)"|
+    assert sprite_sheet =~ ~s|marker-end="url(##{sprite_id}-arrow)"|
+  end
+
+  test "build rewrites local href fragments" do
+    svg_source_root = unique_tmp_dir!("href-refs")
+    File.mkdir_p!(Path.join(svg_source_root, "icons"))
+
+    File.write!(
+      Path.join(svg_source_root, "icons/links.svg"),
+      [
+        ["<svg xmlns", ?:, "xlink=\"http://www.w3.org/1999/xlink\" viewBox=\"0 0 24 24\">\n"],
+        "  <defs>\n",
+        "    <g id=\"shape\">\n",
+        "      <path d=\"M0 0h24v24H0z\" />\n",
+        "    </g>\n",
+        "  </defs>\n",
+        ["  <use href=\"", ?#, "shape\" />\n"],
+        ["  <use xlink", ?:, "href=\"", ?#, "shape\" />\n"],
+        "</svg>\n"
+      ]
+    )
+
+    sprite_sheet = SpriteSheet.build(["icons/links"], source_root: svg_source_root)
+    sprite_id = Source.sprite_id("icons/links", svg_source_root)
+
+    assert sprite_sheet =~ ~s(id="#{sprite_id}-shape")
+    assert sprite_sheet =~ ~s(href="##{sprite_id}-shape")
+    assert sprite_sheet =~ ~s(xlink:href="##{sprite_id}-shape")
+  end
+
+  test "build raises for unsupported reference forms" do
+    svg_source_root = unique_tmp_dir!("unsupported-refs")
+    File.mkdir_p!(Path.join(svg_source_root, "icons"))
+
+    File.write!(
+      Path.join(svg_source_root, "icons/external.svg"),
+      """
+      <svg viewBox="0 0 24 24">
+        <path fill="url(http://example.com/pattern.svg#paint)" d="M0 0h24v24H0z" />
+      </svg>
+      """
+    )
+
+    assert_raise ArgumentError, ~r/unsupported reference/, fn ->
+      SpriteSheet.build(["icons/external"], source_root: svg_source_root)
+    end
+  end
+
   defp count_occurrences(haystack, needle) do
     haystack
     |> String.split(needle)
